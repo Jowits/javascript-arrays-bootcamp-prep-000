daef15ae40a5cc53edec7172eeaf00653d57a4ff
Jowita Lapies <jowita.lapies@gmail.com>
2019-04-04 14:54:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,6 +15,6 @@ function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
   chocolateBars.push(candyString);
   return chocolateBars;
 }
-function accessElementInArray(chocolateBars, [0]) {
-  console.log(chocolateBars[0]);
+function accessElementInArray(array, index) {
+  return array[index];
 }
