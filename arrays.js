21b10caaa8dd18f7130e623175219418006c34e3
Jowita Lapies <jowita.lapies@gmail.com>
2019-04-04 14:26:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = [snickers, hundred grand, kitkat, skittles]
+
+function addElementToBeginningOfArray(arr, n){
+  arr = []
+  return arr.unshift()
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, n) {
+  arr = []
+}
