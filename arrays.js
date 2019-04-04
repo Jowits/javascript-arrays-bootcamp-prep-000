88368783904a91efeaad6ab2a820a2e8ed843480
Jowita Lapies <jowita.lapies@gmail.com>
2019-04-04 15:04:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,6 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
 
 }
 function removeElementFromEndOfArray(chocolateBars) {
-  chocolateBars.slice(0, chocolateBars.length - 1)
-  return chocolateBars;
+  return chocolateBars.slice(0, chocolateBars.length - 1);
+  
 }
